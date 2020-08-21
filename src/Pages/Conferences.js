26034c52383f2cad7ac905f1d5429849abf186cc
senior_lancer <senior_lancer@protonmail.com>
2020-08-22 00:58:46 +1000

    add feature to create room

--- src/Pages/Conferences.js
+++ src/Pages/Conferences.js
@@ -28,15 +28,28 @@ const Conferences = (props) => {
 
     const domain = 'meet.jit.si';
     
+    let room = null;
+    let isJoined = false;
+    let connection = null;
+
+    let localTracks = [];
+
+    const remoteTracks = {};
+
     const options = {
+        // serviceUrl:'wss://meet.jit.si/xmpp-websocket',
         hosts: {
-            domain: 'https://beta.meet.jit.si',
-            muc: 'beta.meet.jit.si', // FIXME: use XEP-0030
+            domain: 'meet.jit.si',
+            muc: 'conference.meet.jit.si', // FIXME: use XEP-0030
+            focus: 'focus.meet.jit.si',
         },
-        bosh: '//beta.meet.jit.si/http-bind', // FIXME: use xep-0156 for that
-    
-        // The name of client node advertised in XEP-0115 'c' stanza
-        clientNode: 'https://beta.meet.jit.si',
+        bosh: 'https://meet.jit.si/http-bind', // FIXME: use xep-0156 for that
+        clientNode: "https://jitsi.org/jitsimeet",
+        // useStunTurn: true
+    };
+
+    const confOptions = {
+        openBridgeChannel: true
     };
 
     useEffect(() => {
@@ -49,14 +62,15 @@ const Conferences = (props) => {
 
             window.$ = $
             window.jQuery = $
-
-            var connection = new window.JitsiMeetJS.JitsiConnection(null, null, options);
+            connection = new window.JitsiMeetJS.JitsiConnection(null, null, options);
 
             connection.addEventListener(window.JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
             connection.addEventListener(window.JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
             connection.addEventListener(window.JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
 
             connection.connect();
+
+            
 
             // window.JitsiMeetJS.mediaDevices.enumerateDevices((devices) => {
             //     setDevices(devices);
@@ -92,15 +106,94 @@ const Conferences = (props) => {
     }, [devices]);
 
     const onConnectionSuccess = () => {
-        console.log('connected');
+        console.log('############################');
+        var roomName = 'conference';
+        
+        room = connection.initJitsiConference('conference', confOptions);
+
+        room.on(window.JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
+        room.on(window.JitsiMeetJS.events.conference.TRACK_REMOVED, track => {
+            console.log(`track removed!!!${track}`);
+        });
+
+        room.on(window.JitsiMeetJS.events.conference.CONFERENCE_JOINED, onConferenceJoined);
+        room.on(window.JitsiMeetJS.events.conference.USER_JOINED, id => {
+            console.log('user join');
+            remoteTracks[id] = [];
+        });
+
+        room.on(window.JitsiMeetJS.events.conference.USER_LEFT, onUserLeft);
+        room.on(window.JitsiMeetJS.events.conference.TRACK_MUTE_CHANGED, track => {
+            console.log(`${track.getType()} - ${track.isMuted()}`);
+        });
+        room.on(window.JitsiMeetJS.events.conference.DISPLAY_NAME_CHANGED, (userID, displayName) => console.log(`${userID} - ${displayName}`));
+        room.on(window.JitsiMeetJS.events.conference.TRACK_AUDIO_LEVEL_CHANGED, (userID, audioLevel) => console.log(`${userID} - ${audioLevel}`));
+        room.on(window.JitsiMeetJS.events.conference.PHONE_NUMBER_CHANGED,() => console.log(`${room.getPhoneNumber()} - ${room.getPhonePin()}`));
+
+        room.join();
     }
 
     const onConnectionFailed = () => {
-        console.log('fail');
+        console.error('Connection Failed!');
     }
 
     const disconnect = () => {
-        console.log('disconnect');
+        console.log('disconnect!');
+        connection.removeEventListener(window.JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
+        connection.removeEventListener(window.JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
+        connection.removeEventListener(window.JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
+    }
+
+    function onRemoteTrack(track) {
+        if (track.isLocal()) {
+            return;
+        }
+        const participant = track.getParticipantId();
+    
+        if (!remoteTracks[participant]) {
+            remoteTracks[participant] = [];
+        }
+        const idx = remoteTracks[participant].push(track);
+    
+        track.addEventListener(window.JitsiMeetJS.events.track.TRACK_AUDIO_LEVEL_CHANGED,
+            audioLevel => console.log(`Audio Level remote: ${audioLevel}`));
+        track.addEventListener(window.JitsiMeetJS.events.track.TRACK_MUTE_CHANGED,
+            () => console.log('remote track muted'));
+        track.addEventListener(window.JitsiMeetJS.events.track.LOCAL_TRACK_STOPPED,
+            () => console.log('remote track stoped'));
+        track.addEventListener(window.JitsiMeetJS.events.track.TRACK_AUDIO_OUTPUT_CHANGED,
+            deviceId =>
+                console.log(
+                    `track audio output device was changed to ${deviceId}`));
+        const id = participant + track.getType() + idx;
+    
+        if (track.getType() === 'video') {
+            $('body').append(
+                `<video autoplay='1' id='${participant}video${idx}' />`);
+        } else {
+            $('body').append(
+                `<audio autoplay='1' id='${participant}audio${idx}' />`);
+        }
+        track.attach($(`#${id}`)[0]);
+    }
+
+    function onConferenceJoined() {
+        isJoined = true;
+        for (let i = 0; i < localTracks.length; i++) {
+            room.addTrack(localTracks[i]);
+        }
+    }
+
+    function onUserLeft(id) {
+        console.log('user left');
+        if (!remoteTracks[id]) {
+            return;
+        }
+        const tracks = remoteTracks[id];
+    
+        for (let i = 0; i < tracks.length; i++) {
+            tracks[i].detach($(`#${id}${tracks[i].getType()}`));
+        }
     }
 
     return(
