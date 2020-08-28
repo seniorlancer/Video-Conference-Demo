ca6f8c220e810ba9e86d560fb520f009dbf9a0d9
senior_lancer <senior_lancer@protonmail.com>
2020-08-28 18:34:55 +1000

    fix bug to show remote small view

--- src/Components/RemoteSmallView/VideoSmallView.js
+++ src/Components/RemoteSmallView/VideoSmallView.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState, Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import PanToolIcon from '@material-ui/icons/PanTool';
 import * as $ from 'jquery';
 import './remotesmallview.css';
 
@@ -7,19 +8,19 @@ export default class VideoSmallView extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {track:this.props.track, video_tag_id: this.props.video_tag_id, user_name:this.props.user_name, overView: false};
+        this.state = { track: this.props.track, video_tag_id: this.props.video_tag_id, user_name: this.props.user_name, overView: false };
         this.handleMouseOver = this.handleMouseOver.bind(this);
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
         this.handleClickSmallVideo = this.handleClickSmallVideo.bind(this);
     }
     handleMouseOver() {
-        this.setState({overView: true});
+        this.setState({ overView: true });
     }
     handleMouseLeave() {
-        this.setState({overView: false});
+        this.setState({ overView: false });
     }
     handleClickSmallVideo() {
-        this.state.track.attach($(`#mainVideo`)[0]);      
+        this.state.track.attach($(`#mainVideo`)[0]);
     }
 
     componentDidMount() {
@@ -31,9 +32,10 @@ export default class VideoSmallView extends Component {
     }
 
     render() {
-        return(
+        return (
             <div id={'div' + this.state.video_tag_id} className="root" onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave} onClick={this.handleClickSmallVideo}>
-                <video className="video" autoPlay='1' id={this.state.video_tag_id} playsInline height='150' width='200' />
+                <video className="video" autoPlay='1' id={this.state.video_tag_id} playsInline height='150' width='200' />   
+                <PanToolIcon className="hand" />             
                 <div className={this.state.overView ? "over_div_show" : "over_div_hide"} >
                     <div className="div_text">{this.state.user_name}</div>
                 </div>
